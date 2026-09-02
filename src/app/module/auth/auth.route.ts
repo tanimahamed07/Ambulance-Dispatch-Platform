68ddc18b0454bfebe2c4ed75d54b850cc8ef5632
tanimahmed07 <tanimislamt@gmail.com>
2026-09-02 14:35:53 +0600

    feat: add Zod validation schemas for user registration, email verification, login, forgot password, and reset password

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -2,14 +2,29 @@ import { Router } from "express";
 import { Role } from "../../../generated/prisma/enums";
 import { auth } from "../../middleware/checkAuth";
 import { AuthController } from "./auth.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { UserValidation } from "./authValidation";
 
 const router = Router();
 
-router.post("/register", AuthController.registerPatient);
+router.post(
+  "/register",
+  validateRequest(UserValidation.CallerRegistrationZodSchema),
+  AuthController.registerPatient,
+);
 
-router.post("/verify-email", AuthController.verifyCallerEmail);
+router.post(
+  "/verify-email",
+  validateRequest(UserValidation.CallerEmailVerifyZodSchema),
+  AuthController.verifyCallerEmail,
+);
 
-router.post("/login", AuthController.loginUser);
+router.post(
+  "/login",
+  validateRequest(UserValidation.LoginZodSchema),
+  AuthController.loginUser,
+);
+
 router.get(
   "/me",
   auth(Role.ADMIN, Role.CALLER, Role.DRIVER, Role.DISPATCHER, Role.ADMIN),
@@ -20,11 +35,13 @@ router.post("/google", AuthController.googleLogin);
 
 router.post(
   "/forgot-password",
+  validateRequest(UserValidation.ForgotPasswordZodSchema),
   AuthController.forgotPassword,
 );
 
 router.post(
   "/reset-password",
+  validateRequest(UserValidation.ResetPasswordZodSchema),
   AuthController.resetPassword,
 );
 
